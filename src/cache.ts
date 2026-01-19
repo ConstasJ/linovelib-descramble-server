7682a1682d27270c95226b3326ee6864c2971346
ConstasJ <constasj@qq.com>
2026-01-19 22:53:40 +0800

    feat: 增加缓存持久化功能

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,6 @@
 import { NovelItem } from "./types";
+import { existsSync } from "node:fs";
+import { readFile, writeFile, mkdir, stat, rm } from "node:fs/promises";
 
 const novelsCache: Array<NovelItem> = [];
 
@@ -28,5 +30,40 @@ export function searchNovelsInCache(query: string): NovelItem[] {
         return keywordsToNovelsMap.get(query)!.novels;
     } else {
         return [];
+    }
+}
+
+const dataDir = process.env.DATA_DIR || "./data";
+
+async function createDataDirIfNotExists(): Promise<void> {
+    if (!existsSync(dataDir)) {
+        await mkdir(dataDir, { recursive: true });
+    }
+    if (existsSync(dataDir) && !(await stat(dataDir)).isDirectory()) {
+        await rm(dataDir);
+        await mkdir(dataDir, { recursive: true });
+    }
+}
+
+export async function saveCache(): Promise<void> {
+    await createDataDirIfNotExists();
+    const cacheFilePath = `${dataDir}/novelsCache.json`;
+    const cacheData = {
+        lastUpdate: Date.now(),
+        novelsCache,
+        keywordsToNovelsMap: Array.from(keywordsToNovelsMap.entries()),
+    };
+    await writeFile(cacheFilePath, JSON.stringify(cacheData), "utf-8");
+}
+
+export async function loadCache(): Promise<void> {
+    const novelCacheFilePath = `${dataDir}/novelsCache.json`;
+    if (existsSync(novelCacheFilePath)) {
+        const fileData = await readFile(novelCacheFilePath, "utf-8");
+        const cacheData = JSON.parse(fileData);
+        novelsCache.push(...cacheData.novelsCache);
+        for (const [key, value] of cacheData.keywordsToNovelsMap) {
+            keywordsToNovelsMap.set(key, value);
+        }
     }
 }
